9afacad728c4dfea7f55c6eba64e1d128d9c368e
Kiril Gaydarov <kvg1@alu.ua.es>
2019-05-14 16:39:41 +0200

    Añadido firebase

--- RedSports/src/app/login/login.page.ts
+++ RedSports/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app'
 
 @Component({
   selector: 'app-login',
@@ -12,4 +13,10 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
+  email: string
+  pass: string
+ 
+  goToHome() {
+    //firebase.auth()
+  }
 }
