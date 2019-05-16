e012b13220c56db0ea94eb52707694bc3d114a2f
Wing Kei Liang Tan <wklt1@alu.ua.es>
2019-05-16 11:10:28 +0200

    Añadido invitar usuario a evento

--- RedSports/src/app/evento/evento.page.ts
+++ RedSports/src/app/evento/evento.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import * as firebase from "firebase";
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-evento',
@@ -19,7 +20,7 @@ export class EventoPage implements OnInit {
   public user: string = "gb8KcNeo7dZXUyhWmGhmHAYjosu2"
   public idEvento: string
   
-  constructor(private router: Router, private route: ActivatedRoute,) { 
+  constructor(private router: Router, private route: ActivatedRoute, public alertCtrl: AlertController) { 
     this.idEvento = this.route.snapshot.paramMap.get('id');
     this.verEvento()
     this.comprobarEventoSubscrito()
@@ -75,7 +76,7 @@ export class EventoPage implements OnInit {
   }
 
   apuntarse() {
-    console.log(this.participar)
+    //console.log(this.participar)
     if(this.participar == 'Desapuntarse') {
       this.buttonColor = "danger"
       this.participar = "Desapuntar"
@@ -94,6 +95,76 @@ export class EventoPage implements OnInit {
         this.router.navigateByUrl('tabs/eventos')
       }
     })
+  } 
+
+  doPrompt() {
+    let prompt = this.alertCtrl.create({
+      header: 'Invitar a evento',
+      message: "Introduce el usuario al que quieres invitar",
+      inputs: [
+        {
+          name: 'usuario',
+          placeholder: 'usuario'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+          }
+        },
+        {
+          text: 'Invitar',
+          handler: data => {
+            this.invitarUsuario(data)
+          }
+        }
+      ]
+    });
+
+    prompt.then((_prompt: any)=> {
+      _prompt.present(); 
+    })
+  }
+
+  invitarUsuario(data: any) {
+    var key= firebase.database().ref('notificaciones/').push().key
+    this.ref = firebase.database().ref('users/')
+      this.ref.on('value', usuarios => {
+        usuarios.forEach(usuario => {
+          if(usuario.val().nombre == data.usuario) {
+            this.ref = firebase.database().ref('notificaciones/'+key)
+            this.ref.update({ 
+              texto: usuario.val().nombre+" te ha invitado a un evento",
+              leido: false,
+              evento: this.idEvento
+            })
+            firebase.database().ref('users/'+usuario.key+"/notificaciones/").update({ [key]: true })
+            this.showAlert()
+          }
+      });
+    })
   }
   
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      header: 'Invitación enviada!',
+      subHeader: 'La invitación al evento ha sido enviada correctamente',
+      buttons: ['OK']
+    });
+    alert.then((_alert: any)=> {
+      _alert.present(); 
+    })
+  }
+  /*
+  invitarUsuario(data: any) {
+    this.ref = firebase.database().ref('users/')
+      this.ref.on('value', usuarios => {
+        usuarios.forEach(usuario => {
+          if(usuario.val().nombre == data.usuario && ok) {
+            this.ref = firebase.database().ref('users/'+usuario.key).child('notificaciones').push({texto: true})
+          }
+      });
+    })  
+  }*/
 }
