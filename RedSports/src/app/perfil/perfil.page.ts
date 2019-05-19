8263604333ae8fbf2cbb5609096f5a85626321d0
Alberto Joaquín López Sellers <ajls1@alu.ua.es>
2019-05-20 01:14:29 +0200

    Diseño basico perfil

--- RedSports/src/app/perfil/perfil.page.ts
+++ RedSports/src/app/perfil/perfil.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticateService } from '../services/authentication.service';
+import { AngularFireDatabase } from "@angular/fire/database";
 import { Router } from '@angular/router';
 
 @Component({
@@ -8,13 +9,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./perfil.page.scss'],
 })
 export class PerfilPage implements OnInit {
+  ref
+  usuarioPerfil: any = {}
+  public user: string = "gb8KcNeo7dZXUyhWmGhmHAYjosu2"
 
   constructor(
     private router: Router,
-    private authService: AuthenticateService
+    private authService: AuthenticateService,
+    public fbd:AngularFireDatabase
   ) { }
 
   ngOnInit() {
+    this.ref = this.fbd.database.ref('users/'+this.user)
+    this.ref.on('value', usuarioPerfil => { 
+      if(usuarioPerfil.exists()) {
+        this.usuarioPerfil.user= this.user
+        this.usuarioPerfil.nombre = "aaa"//usuarioPerfil.val().nombre 
+        this.usuarioPerfil.descripcion = "aaa"//usuarioPerfil.val().descripcion
+        this.usuarioPerfil.seguidores = 12//usuarioPerfil.val().seguidores
+      }
+    });
   }
 
   userProfile() {
