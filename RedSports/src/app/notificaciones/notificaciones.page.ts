866c88792bb65dd1b4e329cb216d2ca2e2d1320f
amr139 <31918471+amr139@users.noreply.github.com>
2019-05-19 21:07:37 +0200

    cambios notificaciones

--- RedSports/src/app/notificaciones/notificaciones.page.ts
+++ RedSports/src/app/notificaciones/notificaciones.page.ts
@@ -12,6 +12,9 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class NotificacionesPage implements OnInit {
     
+    itemsPorLeer = []
+    itemsLeidos = []
+
     constructor(
         public navCtrl: NavController,
         private router: Router,
@@ -20,8 +23,15 @@ export class NotificacionesPage implements OnInit {
         public auth:AngularFireAuth
     ) {}
 
-    ngOnInit() {
-        console.log(this.auth.auth.currentUser);
+    ngOnInit() {}
+
+    ionViewDidEnter(){
+        this.itemsPorLeer = []
+        this.itemsLeidos = []
+        if(this.auth.auth.currentUser)
+            this.getNotices();
+        else
+            this.router.navigateByUrl('/')
     }
 
     search() {
@@ -31,8 +41,6 @@ export class NotificacionesPage implements OnInit {
     userProfile() {
       this.router.navigateByUrl('/tabs/perfil')
     }
-
-    
 
     logout() {
         this.authService.logoutUser()
@@ -46,19 +54,22 @@ export class NotificacionesPage implements OnInit {
     }
 
     getNotices(){
-        let ref = this.fbd.database.ref('notificaciones/').orderByChild('para').equalTo(this.auth.auth.currentUser.uid)
-                /*ref.on('value', eventosUsuarios => {
-                    eventosUsuarios.forEach(eventoUsuario => {
-                        this.fbd.database.ref('eventos/' + eventoUsuario.key + '/').on('value', infoEvento => {
-                            let evento = infoEvento.val()
-                            if(evento){
-                                evento.key = eventoUsuario.key
-                            this.items.push(evento)
-                            }
-                        })
-                    });
-                    this.itemsFiltrado = this.items;
-                })*/
+        console.log('cargo noticias');
+        let nodo = this.fbd.database.ref('notificaciones/').orderByChild('para').equalTo(this.auth.auth.currentUser.uid);
+        nodo.on('value', listaNoticias => {
+            console.log(listaNoticias)
+            listaNoticias.forEach( noticia => {
+                let notice = noticia.val();
+                if(notice){
+                    notice.key = noticia.key
+                    if(notice.leido)
+                        this.itemsLeidos.push(notice)
+                    else 
+                        this.itemsPorLeer.push(notice)
+                }               
+            })                               
+        })
+        
     }
-
 }
+
