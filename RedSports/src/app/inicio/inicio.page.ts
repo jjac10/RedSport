ddf58e43b0411a12a9b88adc5ef99950ee6dc13a
Kiril Gaydarov <kvg1@alu.ua.es>
2019-05-20 03:37:25 +0200

    Casi implementado añadir post

--- RedSports/src/app/inicio/inicio.page.ts
+++ RedSports/src/app/inicio/inicio.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticateService } from '../services/authentication.service';
 import { Router } from '@angular/router';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { AlertController } from '@ionic/angular';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase';
 
 @Component({
   selector: 'app-inicio',
@@ -14,6 +17,8 @@ export class InicioPage implements OnInit {
   constructor(
     private router: Router,
     private authService: AuthenticateService,
+    private alertCtrl: AlertController,
+    private auth: AngularFireAuth,
     private fbd: AngularFireDatabase
   ) { }
 
@@ -23,6 +28,50 @@ export class InicioPage implements OnInit {
         this.posts.push(post.val())
       })
     })
+  }
+
+  doPrompt() {
+    let prompt = this.alertCtrl.create({
+      header: 'Añadir comentario',
+      inputs: [
+        {
+          name: 'texto',
+          placeholder: 'Comentario...'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+          }
+        },
+        {
+          text: 'Enviar',
+          handler: data => {
+            this.crearComentario(data)
+          }
+        }
+      ]
+    })
+
+    prompt.then((_prompt: any)=> {
+      _prompt.present(); 
+    })
+  }
+
+  crearComentario(data: any) {
+    var user = this.fbd.database.ref('users/' + this.fbd.database.app.auth().currentUser.uid)
+    .on('value', usuario => {
+      console.log(usuario)
+    })
+
+    console.log(user)
+
+    data.user = this.fbd.database.app.auth().currentUser.uid
+    data.fecha = new Date().toLocaleString().replace(",","")
+    console.log(data.fecha)
+
+    this.fbd.database.ref('posts/').push(data)
   }
 
   userProfile() {
